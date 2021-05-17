7f654bcdbe40490890725e088a7edaae1e028e36
mikedigan8 <mcdigan@mavs.coloradomesa.edu>
2021-05-16 23:10:42 -0600

    Committing

---
+++ auth/client/src/components/ShowBookList.js
@@ -0,0 +1,100 @@
+import React, { Component } from 'react';
+import '../App.css';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import BookCard from './BookCard';
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { logoutUser } from "../actions/authActions";
+
+class ShowBookList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      books: []
+    };
+  }
+
+  onLogoutClick = e => {
+    e.preventDefault();
+    this.props.logoutUser();
+  };
+
+  componentDidMount() {
+    axios
+      .get('http://localhost:3000/api/books')
+      .then(res => {
+        this.setState({
+          books: res.data
+        })
+      })
+      .catch(err =>{
+        console.log('Error from ShowBookList');
+        console.log(err);
+      })
+  };
+
+
+  render() {
+    const books = this.state.books;
+    console.log("PrintBook: " + books);
+    let bookList;
+
+    if(!books) {
+      bookList = "there is no book record!";
+    } else {
+      bookList = books.map((book, k) =>
+        <BookCard book={book} key={k} />
+      );
+    }
+
+    return (
+      <div className="ShowBookList">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <br />
+              <h2 className="display-4 text-center">Books List</h2>
+            </div>
+
+            <div className="col-md-11">
+              <Link to="/create-book" className="btn btn-outline-warning float-right">
+                + Add New Book
+              </Link>
+              <br />
+              <br />
+              <hr />
+            </div>
+
+          </div>
+
+          <div className="list">
+                {bookList}
+          </div>
+        </div>
+        <footer>
+        <button
+              style={{
+                width: "150px",
+                borderRadius: "3px",
+                letterSpacing: "1.5px",
+                marginTop: "1rem"
+              }}
+              onClick={this.onLogoutClick}
+              className="btn btn-large waves-effect waves-light hoverable blue accent-3"
+            >
+              Logout
+            </button>
+        </footer>
+      </div>
+    );
+  }
+}ShowBookList.propTypes = {
+  logoutUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired
+};const mapStateToProps = state => ({
+  auth: state.auth
+});export default connect(
+  mapStateToProps,
+  { logoutUser }
+)(ShowBookList);
---
+++ auth/config/keys.js
@@ -0,0 +1,4 @@
+module.exports = { 
+    mongoURI: "mongodb+srv://test-user:C8d7ZjGRIfj9tuwq@cluster0.tnfws.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    secretOrKey: "secret"
+};
